3e5f7cb21241d2627421a133eed1157cc0cb682c
peterdyer7 <peter_dyer@hotmail.com>
2019-02-27 16:23:22 -0500

    added process-image (and required updates)

--- functions/index.js
+++ functions/index.js
@@ -4,3 +4,10 @@ if (
 ) {
   exports.mediaLibraryFunction = require('./media-library-function');
 }
+
+if (
+  !process.env.FUNCTION_NAME ||
+  process.env.FUNCTION_NAME === 'processImage'
+) {
+  exports.processImage = require('./process-image');
+}
